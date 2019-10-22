2603787d2c1706412cb96983865ee845543836e8
alicja99 <alicja.mruk99@gmail.com>
2019-10-22 10:22:10 +0200

    Client change
    added function getLiczby() to get userInput
    switch case statement, when user inputs valid option - need to be impemented on server - response with stat#ERROR

--- Klient/src/Operacja.java
+++ Klient/src/Operacja.java
@@ -1,18 +1,30 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Operacja {
     private static int wybor;
-    private static String  OPERACJA = "oper#";
-    private static String STATUS = "stat#NULL@";
-    private static String IDEN  = "iden#";
-    private static String dzialanie;
     private static String komunikat;
+    private static Scanner userEntry;
+    private static boolean dzielenie;
 
-    private int liczba1, liczba2, liczba3;
+    private static String OPERACJA = "oper#";
+    private static String STATUS = "stat#NULL@";
+    private static String IDEN = "iden#";
+
     public Operacja() {
-        komunikat="";
+        dzielenie = false;
+        komunikat = "";
     }
-    public static void pokazMenu(){
+    private void setDefaults(){
+        OPERACJA = "oper#";
+        IDEN = "iden#";
+    }
+    private static void setDefaultTextOfStatement(){
+        OPERACJA = "oper#";
+        IDEN = "iden#";
+    }
+
+    public static void pokazMenu() {
         System.out.println("Jaka operacja? ");
         System.out.println("--------------- ");
         System.out.println("1. Dodawanie ");
@@ -22,46 +34,57 @@ public class Operacja {
         System.out.println("-----------------");
 
     }
-    public static int getWybor(){
+
+    public static int getWybor() {
         return wybor;
     }
-    public static String getKomunikat(int wybor){
 
-        boolean dzielenie = false;
+    public static String getKomunikat(int wybor) {
 
-        Scanner userEntry = new Scanner(System.in);
-        wybor= userEntry.nextInt();
+        userEntry = new Scanner(System.in);
+        wybor = userEntry.nextInt();
+        IDEN += Czas.getGodzina() + "##"; //doklejenie do iden godziny //USUNĄĆ JEDNEGO HASHA..?
 
-        switch(wybor){
-            case 1: {
-                //dodawanie
-                dzialanie+="dodawanie@";
-                break;
-            }
-            case 2: {
-                //odejmowanie
-               dzialanie+="odejmowanie@";
-                break;
-            }
-            case 3: {
-                //mnożenie
-                dzialanie+="mnozenie@";
-                break;
-            }
-            case 4: {
-                //dzielenie
-                dzialanie+="dzielenie@";
-                dzielenie = true;
-                break;
-            }
-            default: System.out.println("Takiej opcji nie ma. Wpisz jeszcze raz");
+    switch (wybor) {
+        case 1: {
+            //dodawanie
+            OPERACJA += "dodawanie@";
+            getLiczby();
             break;
         }
-        //USUNĄĆ JEDNEGO HASHA..?
-        IDEN += Czas.getGodzina()+ "##"; //doklejenie do iden godziny
+        case 2: {
+            //odejmowanie
+            OPERACJA += "odejmowanie@";
+            getLiczby();
+            break;
+        }
+        case 3: {
+            //mnożenie
+            OPERACJA += "mnozenie@";
+            getLiczby();
+            break;
+        }
+        case 4: {
+            //dzielenie
+            OPERACJA += "dzielenie@";
+            dzielenie = true;
+            getLiczby();
+            break;
+        }
+        default:
+            System.out.println("Zły wybór. Wpisz ponownie od 1 do 4");
+    }
+
+
+
+        komunikat = OPERACJA + STATUS + IDEN;
+        setDefaultTextOfStatement();
+        return komunikat;
+    }
+    private static void getLiczby(){
         System.out.println("Podaj trzy liczby");
 
-        for (int i = 0; i < 3;) {
+        for (int i = 0; i < 3; ) {
             //wprowadzanie liczby do zmiennej tymczasowej
             wybor = userEntry.nextInt();
             //warunek sprawdzajacy czy dana operacja jest dzieleniem lub, w przypadku dzielenia, sprawdza czy aktualnie ustawiana jest pierwsza liczba (w przypadku dzielenia moze byc to 0)
@@ -70,26 +93,22 @@ public class Operacja {
                 IDEN += Integer.toString(wybor);
                 IDEN += (i != 2) ? "#" : "@";
                 i++;
-            }
-            else {
+            } else {
                 //wykrycie czy dana operacja jest dzieleniem
                 if (dzielenie) {
                     //wykrycie czy zostalo wpisane 0 dla drugiej lub trzeciej liczby, jesli nie, liczba zostanie dopisana, jesli tak, krok dopisania zostanie pominiety
-                    if ( wybor!= 0) {
+                    if (wybor != 0) {
                         IDEN += Integer.toString(wybor);
                         IDEN += (i != 2) ? "#" : "@";
                         i++;
-                    }
-                    else {
+                    } else {
                         System.out.println("To nie może być zero! Wpisz jesczcze raz");
                     }
                 }
             }
         }
-        komunikat = OPERACJA + STATUS + IDEN;
-        return komunikat;
     }
-
-
-
 }
+
+
+
