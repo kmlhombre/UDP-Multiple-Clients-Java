582d5bcdc9a89c282b7cee1426971a8427449d21
alicja99 <alicja.mruk99@gmail.com>
2019-10-24 17:10:42 +0200

    Time and score on client display

--- Klient/src/Czas.java
+++ Klient/src/Czas.java
@@ -3,9 +3,24 @@ import java.util.Calendar;
 public class Czas {
     public Czas() {
     }
-
-    static String getGodzina() {
+    public static String getGodzina(){
         Calendar now = Calendar.getInstance();
-        return (now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE));
+        String time ="";
+        int h = now.get(Calendar.HOUR_OF_DAY);
+        int m = now.get(Calendar.MINUTE);
+        if(h < 10) {
+            time += "0" + h;
+        }
+        else {
+            time += h;
+        }
+        time += ":";
+        if(m < 10) {
+            time += "0" + m;
+        }
+        else {
+            time += m;
+        }
+        return time;
     }
 }
